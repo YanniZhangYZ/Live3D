13012279ebec7665e936a03c02f673748890684a
YanniZhangYZ <yanni.zhang.github@outlook.com>
2023-09-12 15:55:56 +0200

    Add the light weight verison of all the encoders

--- E_high.py
+++ E_high.py
@@ -27,3 +27,32 @@ class EncoderHigh(nn.Module):
         x = self.conv5(x)
         x = F.leaky_relu(x,negative_slope=0.01)
         return x
+
+class EncoderHighLT(nn.Module):
+    # This is the light version of module that encodes the input original image to a feature map with high frequency
+    # The structure implemented here is the same as the one used in the live portrait paper Fig.A11
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(64,128,kernel_zie = (3,3), stride = (2,2), padding = (1,1))
+        self.conv2 = nn.Conv2d(128,128,kernel_size = (3,3), stride = (1,1), padding = (1,1))
+        self.conv3 = nn.Conv2d(128,128,kernel_size = (3,3), stride = (1,1), padding = (1,1))
+        self.conv4 = nn.Conv2d(128,128,kernel_size = (3,3), stride = (1,1), padding = (1,1))
+        self.conv5 = nn.Conv2d(128,128,kernel_size = (3,3), stride = (1,1), padding = (1,1))
+        self.conv6 = nn.Conv2d(128,128,kernel_size = (3,3), stride = (1,1), padding = (1,1))
+        
+       
+        
+        
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.leaky_relu(x,negative_slope=0.01)
+        x = self.conv2(x)
+        x = F.leaky_relu(x,negative_slope=0.01)
+        x = self.conv3(x)
+        x = F.leaky_relu(x,negative_slope=0.01)
+        x = self.conv4(x)
+        x = F.leaky_relu(x,negative_slope=0.01)
+        x = self.conv5(x)
+        x = F.leaky_relu(x,negative_slope=0.01)
+        x = self.conv6(x)
+        return x
--- F_F_Low_ViT.py
+++ F_F_Low_ViT.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class FFHighEncoder(nn.Module):
+class FFLowEncoder(nn.Module):
     # This is the encoder used to get feature map F from F_low
     # The structure implemented here is the same as the one used in the live portrait paper Fig.A10
     def __init__(self):
@@ -39,4 +39,33 @@ class FFHighEncoder(nn.Module):
         x = self.conv2(x)
         x = F.relu(x)
         x = self.conv3(x)
+        return x
+
+
+class FFLowEncoderLT(nn.Module):
+    # This is the light weight version of encoder used to get feature map F from F_low
+    # The structure implemented here is the same as the one used in the live portrait paper Fig.A10
+    def __init__(self):
+        super().__init__()
+        # patch_size shuold be 3
+        # self.overlap_patch_embed = OverLapPatchEmbed(img_size = 64, strid = 2, in_chans = 256, embed_dim = 1024)
+        # without dropout, with qkv_bias = True, with layer normalization
+        # self.transformer_block1 = TransformerBlock(dim = 1024, num_heads = 4, mlp_ratio = 2, sr_ratio = 1) 
+        # self.transformer_block2 = TransformerBlock(dim = 1024, num_heads = 4, mlp_ratio = 2, sr_ratio = 1) 	
+        self.pixel_shuffle = nn.PixelShuffle(upscale_factor = 2)
+        self.conv1 = nn.Conv2d(256,128,kernel_size = (3,3), stride = (1,1), padding = (1,1))
+        self.conv2 = nn.Conv2d(128,128,kernel_size = (3,3), stride = (1,1), padding = (1,1))
+        
+
+        
+        
+    def forward(self, x):
+        # x = self.overlap_patch_embed(x)
+        # x = self.transformer_block1(x)
+        # x = self.transformer_block2(x)
+        x = self.pixel_shuffle(x)
+        x = F.upsampling_bilinear2d(x, scale_factor=2)
+        x = self.conv1(x)
+        x = F.leaky_relu(x,negative_slope=0.01)
+        x = self.conv2(x)
         return x
